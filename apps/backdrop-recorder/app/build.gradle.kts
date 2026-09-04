789e33fccd38258931ea4758283d4e7229a1711a
savostyanovlaw <savostyanovlaw@gmail.com>
2026-09-04 13:11:45 -0700

    fix(android): enable BuildConfig for tests

--- apps/backdrop-recorder/app/build.gradle.kts
+++ apps/backdrop-recorder/app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
         debug {
             isMinifyEnabled = false
