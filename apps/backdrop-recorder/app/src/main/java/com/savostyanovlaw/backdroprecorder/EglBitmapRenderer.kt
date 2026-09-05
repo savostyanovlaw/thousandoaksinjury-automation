34d634ed1c15ea402d399fc39201f7778debb7c0
savostyanovlaw <savostyanovlaw@gmail.com>
2026-09-04 17:06:39 -0700

    feat(android): add EGL renderer for recorded composite frames

---
+++ apps/backdrop-recorder/app/src/main/java/com/savostyanovlaw/backdroprecorder/EglBitmapRenderer.kt
@@ -0,0 +1,204 @@
+package com.savostyanovlaw.backdroprecorder
+
+import android.graphics.Bitmap
+import android.opengl.EGL14
+import android.opengl.EGLConfig
+import android.opengl.EGLContext
+import android.opengl.EGLDisplay
+import android.opengl.EGLSurface
+import android.opengl.GLES20
+import android.opengl.GLUtils
+import android.view.Surface
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+
+class EglBitmapRenderer(
+    private val targetSurface: Surface,
+    private val width: Int,
+    private val height: Int,
+) {
+    private var eglDisplay: EGLDisplay = EGL14.EGL_NO_DISPLAY
+    private var eglContext: EGLContext = EGL14.EGL_NO_CONTEXT
+    private var eglSurface: EGLSurface = EGL14.EGL_NO_SURFACE
+    private var program = 0
+    private var textureId = 0
+    private var positionHandle = 0
+    private var textureHandle = 0
+
+    private val vertexBuffer: FloatBuffer = ByteBuffer
+        .allocateDirect(VERTICES.size * 4)
+        .order(ByteOrder.nativeOrder())
+        .asFloatBuffer()
+        .apply {
+            put(VERTICES)
+            position(0)
+        }
+
+    init {
+        setupEgl()
+        setupGl()
+    }
+
+    fun render(bitmap: Bitmap) {
+        if (eglDisplay == EGL14.EGL_NO_DISPLAY) return
+        EGL14.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)
+        GLES20.glViewport(0, 0, width, height)
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        GLES20.glUseProgram(program)
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
+
+        vertexBuffer.position(0)
+        GLES20.glEnableVertexAttribArray(positionHandle)
+        GLES20.glVertexAttribPointer(positionHandle, 2, GLES20.GL_FLOAT, false, STRIDE_BYTES, vertexBuffer)
+
+        vertexBuffer.position(2)
+        GLES20.glEnableVertexAttribArray(textureHandle)
+        GLES20.glVertexAttribPointer(textureHandle, 2, GLES20.GL_FLOAT, false, STRIDE_BYTES, vertexBuffer)
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+        GLES20.glDisableVertexAttribArray(positionHandle)
+        GLES20.glDisableVertexAttribArray(textureHandle)
+        EGL14.eglSwapBuffers(eglDisplay, eglSurface)
+    }
+
+    fun release() {
+        if (eglDisplay != EGL14.EGL_NO_DISPLAY) {
+            EGL14.eglMakeCurrent(
+                eglDisplay,
+                EGL14.EGL_NO_SURFACE,
+                EGL14.EGL_NO_SURFACE,
+                EGL14.EGL_NO_CONTEXT,
+            )
+            if (program != 0) GLES20.glDeleteProgram(program)
+            if (textureId != 0) GLES20.glDeleteTextures(1, intArrayOf(textureId), 0)
+            EGL14.eglDestroySurface(eglDisplay, eglSurface)
+            EGL14.eglDestroyContext(eglDisplay, eglContext)
+            EGL14.eglReleaseThread()
+            EGL14.eglTerminate(eglDisplay)
+        }
+        eglDisplay = EGL14.EGL_NO_DISPLAY
+        eglContext = EGL14.EGL_NO_CONTEXT
+        eglSurface = EGL14.EGL_NO_SURFACE
+        targetSurface.release()
+    }
+
+    private fun setupEgl() {
+        eglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY)
+        check(eglDisplay != EGL14.EGL_NO_DISPLAY) { "Unable to get EGL display" }
+
+        val version = IntArray(2)
+        check(EGL14.eglInitialize(eglDisplay, version, 0, version, 1)) { "Unable to initialize EGL" }
+
+        val configAttributes = intArrayOf(
+            EGL14.EGL_RED_SIZE, 8,
+            EGL14.EGL_GREEN_SIZE, 8,
+            EGL14.EGL_BLUE_SIZE, 8,
+            EGL14.EGL_ALPHA_SIZE, 8,
+            EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+            EGL14.EGL_SURFACE_TYPE, EGL14.EGL_WINDOW_BIT,
+            EGL14.EGL_NONE,
+        )
+        val configs = arrayOfNulls<EGLConfig>(1)
+        val configCount = IntArray(1)
+        check(EGL14.eglChooseConfig(eglDisplay, configAttributes, 0, configs, 0, 1, configCount, 0)) {
+            "Unable to choose EGL config"
+        }
+        val config = requireNotNull(configs[0]) { "No EGL config available" }
+
+        val contextAttributes = intArrayOf(EGL14.EGL_CONTEXT_CLIENT_VERSION, 2, EGL14.EGL_NONE)
+        eglContext = EGL14.eglCreateContext(
+            eglDisplay,
+            config,
+            EGL14.EGL_NO_CONTEXT,
+            contextAttributes,
+            0,
+        )
+        check(eglContext != EGL14.EGL_NO_CONTEXT) { "Unable to create EGL context" }
+
+        val surfaceAttributes = intArrayOf(EGL14.EGL_NONE)
+        eglSurface = EGL14.eglCreateWindowSurface(
+            eglDisplay,
+            config,
+            targetSurface,
+            surfaceAttributes,
+            0,
+        )
+        check(eglSurface != EGL14.EGL_NO_SURFACE) { "Unable to create EGL surface" }
+        check(EGL14.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext)) {
+            "Unable to make EGL context current"
+        }
+    }
+
+    private fun setupGl() {
+        val vertexShader = compileShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER)
+        val fragmentShader = compileShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER)
+        program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, fragmentShader)
+        GLES20.glLinkProgram(program)
+        val linkStatus = IntArray(1)
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0)
+        check(linkStatus[0] == GLES20.GL_TRUE) { "Unable to link recording shader: ${GLES20.glGetProgramInfoLog(program)}" }
+        GLES20.glDeleteShader(vertexShader)
+        GLES20.glDeleteShader(fragmentShader)
+
+        positionHandle = GLES20.glGetAttribLocation(program, "aPosition")
+        textureHandle = GLES20.glGetAttribLocation(program, "aTexCoord")
+        val samplerHandle = GLES20.glGetUniformLocation(program, "uTexture")
+        GLES20.glUseProgram(program)
+        GLES20.glUniform1i(samplerHandle, 0)
+
+        val textures = IntArray(1)
+        GLES20.glGenTextures(1, textures, 0)
+        textureId = textures[0]
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glClearColor(0f, 0f, 0f, 1f)
+    }
+
+    private fun compileShader(type: Int, source: String): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, source)
+        GLES20.glCompileShader(shader)
+        val status = IntArray(1)
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, status, 0)
+        check(status[0] == GLES20.GL_TRUE) { "Unable to compile recording shader: ${GLES20.glGetShaderInfoLog(shader)}" }
+        return shader
+    }
+
+    companion object {
+        private const val STRIDE_BYTES = 4 * 4
+        private val VERTICES = floatArrayOf(
+            -1f, -1f, 0f, 1f,
+             1f, -1f, 1f, 1f,
+            -1f,  1f, 0f, 0f,
+             1f,  1f, 1f, 0f,
+        )
+
+        private const val VERTEX_SHADER = """
+            attribute vec4 aPosition;
+            attribute vec2 aTexCoord;
+            varying vec2 vTexCoord;
+            void main() {
+                gl_Position = aPosition;
+                vTexCoord = aTexCoord;
+            }
+        """
+
+        private const val FRAGMENT_SHADER = """
+            precision mediump float;
+            uniform sampler2D uTexture;
+            varying vec2 vTexCoord;
+            void main() {
+                gl_FragColor = texture2D(uTexture, vTexCoord);
+            }
+        """
+    }
+}
